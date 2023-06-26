d6a720c5e84ae0dcb866b986d67a04abf43398e7
Bert Blommers <info@bertblommers.nl>
2023-06-26 15:47:49 +0000

    TechDebt: Add integration test to verify infra

---
+++ integration_tests/__init__.py
@@ -0,0 +1,2 @@
+# Collection of tests that assume that the infrastructure is set-up
+# Runs against a MotoServer instance
---
+++ integration_tests/test_environment.py
@@ -0,0 +1,33 @@
+import boto3
+
+
+def test_dynamodb():
+    ddb = boto3.client("dynamodb", "us-east-1", endpoint_url="http://localhost:5000")
+    table_names = ddb.list_tables()["TableNames"]
+    assert len(table_names) == 5
+    assert "PullRequests" in table_names
+    assert "UserSettings" in table_names
+    assert "OAuthState" in table_names
+    assert "Payments" in table_names
+    assert "ScriptExecutionInfo" in table_names
+
+
+def test_dynamodb__different_region():
+    ddb = boto3.client("dynamodb", "us-west-1", endpoint_url="http://localhost:5000")
+    assert ddb.list_tables()["TableNames"] == []
+
+
+def test_apigateway():
+    apigw = boto3.client("apigatewayv2", "us-east-1", endpoint_url="http://localhost:5000")
+    apis = apigw.get_apis()["Items"]
+    api_names = [api["Name"] for api in apis]
+    assert api_names == ["payments-api"]
+
+
+def test_cloudfront():
+    cf = boto3.client("cloudfront", "us-east-1", endpoint_url="http://localhost:5000")
+    distributions = cf.list_distributions()["DistributionList"]["Items"]
+    assert len(distributions) == 1
+
+    # S3 and APIGateway
+    assert len(distributions[0]["Origins"]["Items"]) == 2
